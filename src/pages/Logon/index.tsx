d9f4096ca975115406c050cf808d0f21ce827f7a
acaciomartins <acacio.am@gmail.com>
2022-01-10 12:13:16 -0300

    Inclusao componente logon

--- src/pages/Logon/index.tsx
+++ src/pages/Logon/index.tsx
@@ -3,7 +3,26 @@ import './styles.css';
 export default function Logon() {
     return (
         <>
-            <h1 className="titulo">Logon</h1>
+            <div className="container">
+                <div className="logo">Pet Friendly</div>
+                <div className="login-item">
+                    <form className="form form-login">
+                        <div className="form-field">
+                            {/* <label className="user"><span className="hidden">Usuário</span></label> */}
+                            <input id="login-username" type="text" className="form-input" placeholder="Usuário" required />
+                        </div>
+
+                        <div className="form-field">
+                            {/* <label className="lock"><span className="hidden">Senha</span></label> */}
+                            <input id="login-password" type="password" className="form-input" placeholder="Senha" required />
+                        </div>
+
+                        <div className="form-field">
+                            <input type="submit" value="Log in" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
